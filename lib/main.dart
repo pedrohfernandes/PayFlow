2ebdd9a25a48e86ad14216997de9f0fd53412b6c
Pedro Henrique <pedrohfernandes2001@gmail.com>
2023-07-25 15:09:01 -0300

    adição do firebase ao projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'modules/login/login_page.dart';
 import 'shared/themes/themes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AppWidget());
 }
 
